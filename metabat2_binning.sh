1bc28d3ebdcdfbb1d9c6577abb3b3704f3c67bf7
d-golzato <davide.golzato@unitn.it>
2022-04-14 11:09:41 +0200

    Updated and corrected scripts

--- metabat2_binning.sh
+++ metabat2_binning.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#This script will do binning or co-binning according to the output of bowtie2
-coassembly_directory=$var1/      #co-assembly directory
-coassembly_name=$var2            #coassembly name
+#This script will do binning or co-binning according to the presence of the -t flag
+coassembly_directory=$var1/      #Co-assembly directory
+coassembly_name=$var2            #Coassembly name
 bin_type=$var3                   #Binning type
-mem=$var4                        #min contig lenght
-max_p=$var5                      #good contigs percentage
-min_s=$var6                      #min score for a edge
-max_e=$var7                      #max edges
-advanced=$var8                   #advanced parameters
-ncores=$var9                     #number of cores
-assembler=$var10                 #assembler used (default is megahit)
+min_len=$var4                    #Min contig lenght
+max_p=$var5                      #Good contigs percentage
+min_s=$var6                      #Min score for a edge
+max_e=$var7                      #Max edges
+advanced=$var8                   #Advanced parameters
+ncores=$var9                     #Number of cores
+assembler=$var10                 #Assembler used (default is megahit)
 
 
 metabat_dir="/shares/CIBIO-Storage/CM/cmstore/tools/metabat-2.12.1/"
@@ -20,4 +20,4 @@ if [ ! -d $metabat_output_dir ]; then mkdir -p $metabat_output_dir; fi
 
 ${metabat_dir}jgi_summarize_bam_contig_depths --outputDepth ${metabat_output_dir}/depth.txt ${coassembly_directory}/${coassembly_name}/${assembler}/${bin_type}/*.bam
 
-${metabat_dir}metabat2 -m ${mem} -t 8 --unbinned --maxP ${max_p} --minS ${min_s} --maxEdges ${max_e} --seed 0 -i ${coassembly_directory}/${coassembly_name}/${assembler}/filtered_contigs.fa -a ${coassembly_directory}/${coassembly_name}/${assembler}/${bin_type}/bins_dir/depth.txt -o ${metabat_output_dir}/${coassembly_name}_bin ${advanced}
+${metabat_dir}metabat2 -m ${min_len} -t 8 --unbinned --maxP ${max_p} --minS ${min_s} --maxEdges ${max_e} --seed 0 -i ${coassembly_directory}/${coassembly_name}/${assembler}/filtered_contigs.fa -a ${coassembly_directory}/${coassembly_name}/${assembler}/${bin_type}/bins_dir/depth.txt -o ${metabat_output_dir}/${coassembly_name}_bin ${advanced}
